97a0469c04e35c52541ca2e9d5ff128e8bcbbcfb
cllzkynk <cllzkynk@gmail.com>
2022-02-22 22:52:13 +0200

    22/02/2022

--- src/main/java/com/lecture/carrental/repository/UserRepository.java
+++ src/main/java/com/lecture/carrental/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.User;
+import com.lecture.carrental.exception.BadRequestException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.lecture.carrental.exception.ResourceNotFoundException;
@@ -10,9 +14,20 @@ import java.util.Optional;
 
 @Repository
 @Transactional
-public interface UserRepository extends JpaRepository <User, Long>{
+public interface UserRepository extends JpaRepository <User, Long> {
 
 //    @Query("Select u from user where u.email=?1")
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email) throws ResourceNotFoundException;
+
+
+
+    @Modifying
+    @Query("UPDATE User u " +
+            "SET u.firstName = ?2, u.lastName = ?3, u.phoneNumber = ?4, " +
+            "u.email = ?5, u.address = ?6, u.zipCode = ?7 " +
+            "WHERE u.id = ?1")
+    void update(Long id, String firstName, String lastName, String phoneNumber, String email, String address,
+                String zipCode) throws BadRequestException;
 }
+
