f200376d1edd3b29930ba0c08d924b1a54d4f45b
cllzkynk <cllzkynk@gmail.com>
2022-02-26 20:37:20 +0200

    23/02/2022

--- src/main/java/com/lecture/carrental/service/FileDBService.java
+++ src/main/java/com/lecture/carrental/service/FileDBService.java
@@ -1,24 +1,29 @@
 package com.lecture.carrental.service;
-
-
 import com.lecture.carrental.domain.FileDB;
 import com.lecture.carrental.repository.FileDBRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-@Service
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.Stream;
 @AllArgsConstructor
+@Service
 public class FileDBService {
-
     private final FileDBRepository fileDBRepository;
-
-
-    public FileDB getFileById(String id){
-
+    public FileDB store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        FileDB fileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+        fileDBRepository.save(fileDB);
+        return fileDB;
+    }
+    public FileDB getFileById(String id) {
         return fileDBRepository.findById(id).get();
-
     }
 
-
+    public Stream<FileDB> getAllFiles() {
+        return fileDBRepository.findAll().stream();
+    }
 
 }
